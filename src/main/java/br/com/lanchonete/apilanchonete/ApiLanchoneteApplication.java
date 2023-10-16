5a3d441154b7073255bbdae1981bd60c28bec136
matheusjardim-dev <matheusjardim.dev@gmail.com>
2023-10-16 13:31:50 -0300

    initial commit

---
+++ src/main/java/br/com/lanchonete/apilanchonete/ApiLanchoneteApplication.java
@@ -0,0 +1,13 @@
+package br.com.lanchonete.apilanchonete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLanchoneteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLanchoneteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lanchonete/apilanchonete/ApiLanchoneteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lanchonete.apilanchonete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLanchoneteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
