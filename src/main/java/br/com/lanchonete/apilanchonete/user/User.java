7d0f5df1a5c1a0af3a58b9b656a1d0df37d8491d
matheusjardim-dev <matheusjardim.dev@gmail.com>
2023-10-16 15:40:08 -0300

    repositório User

---
+++ src/main/java/br/com/lanchonete/apilanchonete/user/User.java
@@ -0,0 +1,58 @@
+package br.com.lanchonete.apilanchonete.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 16)
+    private Long RA;
+
+    @Column(length = 15, nullable = false)
+    private String password;
+
+    @Column(length = 45, nullable = false, name = "nome")
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Long getRA() {
+        return RA;
+    }
+
+    public void setRA(Long rA) {
+        RA = rA;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
