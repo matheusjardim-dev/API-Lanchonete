ae6a081e21d0ad904aa4e4acdaded0559e602dd2
matheusjardim-dev <matheusjardim.dev@gmail.com>
2023-10-24 15:51:36 -0300

    edit User

--- src/main/java/br/com/lanchonete/apilanchonete/user/UserController.java
+++ src/main/java/br/com/lanchonete/apilanchonete/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -23,13 +24,28 @@ public class UserController {
     @GetMapping("/users/new")
     public String showNewForm(Model model){
         model.addAttribute("user", new User());
+        model.addAttribute("pageTitle", "Cadastrar Usuário");
         return "cadUser";
     }
-
+    
     @PostMapping("/users/save")
     public String saveUser(User user, RedirectAttributes ra){
         service.save(user);
         ra.addFlashAttribute("message", "Usuário cadastrado com sucesso!");
         return "redirect:/users";  
+    }
+    
+    @GetMapping("/users/edit/{id}")
+    public String showEditForm(@PathVariable("id") Integer id, Model model, RedirectAttributes ra){
+        try {
+            User user = service.get(id);
+            model.addAttribute("user", user);
+            model.addAttribute("pageTitle", "Editar Usuário (ID: " + id + ")");
+            return "cadUser";
+        } catch (UserNotFoundException e) {
+            ra.addFlashAttribute("message", "Usuário cadastrado com sucesso!");
+            return "redirect:/users";  
+        }
+
     }
 } 
--- src/main/java/br/com/lanchonete/apilanchonete/user/UserService.java
+++ src/main/java/br/com/lanchonete/apilanchonete/user/UserService.java
@@ -1,6 +1,7 @@
 package br.com.lanchonete.apilanchonete.user;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,13 @@ public class UserService {
 
     public void save(User user) {
         repo.save(user);
+    }
+
+    public User get(Integer id) throws UserNotFoundException{
+        Optional<User> result = repo.findById(id);
+        if(result.isPresent()){
+            return result.get();
+        }
+        throw new UserNotFoundException("Nenhum Usuário foi encontrado com o id " + id);
     }
 }
