1e1fd002ee47c49443f1ee5b48b2c63a553ca1cf
matheusjardim-dev <matheusjardim.dev@gmail.com>
2023-10-24 15:32:22 -0300

    cadastro de usuários

--- src/main/java/br/com/lanchonete/apilanchonete/user/UserController.java
+++ src/main/java/br/com/lanchonete/apilanchonete/user/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserController {
@@ -21,8 +23,13 @@ public class UserController {
     @GetMapping("/users/new")
     public String showNewForm(Model model){
         model.addAttribute("user", new User());
-
-
         return "cadUser";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes ra){
+        service.save(user);
+        ra.addFlashAttribute("message", "Usuário cadastrado com sucesso!");
+        return "redirect:/users";  
     }
 } 
