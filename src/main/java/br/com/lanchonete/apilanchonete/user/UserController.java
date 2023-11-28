7db5e7ae42e80c7e35093f6ddd8ecd6c61ac223d
matheusjardim-dev <matheusjardim.dev@gmail.com>
2023-11-28 07:27:03 -0300

    final commit

--- src/main/java/br/com/lanchonete/apilanchonete/user/UserController.java
+++ src/main/java/br/com/lanchonete/apilanchonete/user/UserController.java
@@ -9,14 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import br.com.lanchonete.apilanchonete.produto.ProdutoService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
     @Autowired private UserService service;
-    @Autowired private ProdutoService prodservice;
     @Autowired
     private HttpServletRequest request;
 
